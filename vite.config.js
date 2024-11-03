94727a6af1748d966c5c5b5a050acfc589867bba
vesavaittinen <156056660+vesavaittinen@users.noreply.github.com>
2024-11-03 14:17:29 +0200

    Some commits

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todolistreact/',
   plugins: [react()],
   test: {
     globals: true,
